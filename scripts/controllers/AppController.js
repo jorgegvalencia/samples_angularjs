eda0655175ad6f0a64505b997a874ddef2ed6fcc
Jorge González <jorge.gonzalez@babel.es>
2016-03-22 09:40:11 +0100

    Titulo de la página según url.

--- scripts/controllers/AppController.js
+++ scripts/controllers/AppController.js
@@ -1,4 +1,12 @@
-angular.module("moviedb").controller("AppController", ["$scope", function($scope) {
+angular.module("moviedb").controller("AppController", ["$scope", "$location", function($scope, $location) {
+	var controller = this;
+
+	// controller properties
+	this.titles = {
+		"/movies": "Movies List",
+		"/series": "Series List",
+		"/people": "People List"
+	}
 
     // model init
     $scope.model = { // es buena practica tener un objeto para el modelo
@@ -6,8 +14,8 @@ angular.module("moviedb").controller("AppController", ["$scope", function($scope
     };
 
     // scope event listeners
-    $scope.$on("OnMenuChange", function (evt, data) {
-    	$scope.model.title = data;
+    $scope.$on("$locationChangeSuccess", function (evt, currentRoute) {
+    	$scope.model.title = controller.titles[$location.path()] || "404 Not Found";
     });
 
 }]);
